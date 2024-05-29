692ec881460213eea9ceadc47b6d17609e55b907
killy.ren <289472438@qq.com>
2024-05-29 12:00:19 +0800

    test

--- Examples/MonoWebViewTest/Program.cs
+++ Examples/MonoWebViewTest/Program.cs
@@ -28,10 +28,8 @@ namespace MonoWebViewTest
             }
             using (var window = new Window())
             {
-                // this relates to the path defined in the .csproj file
                 Application.ContentProvider = new EmbeddedContentProvider("App");
 
-                // runs the application and opens the window with the given page loaded
                 Application.Run(window, "https://www.baidu.com");
             }
         }
